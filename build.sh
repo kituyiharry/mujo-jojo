156319bed8cabb0652c77184e617e0be9cd15d13
Harry <harryk@harryk.dev>
2025-11-29 22:36:01 +0800

    fix build error

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ pushd $(pwd);
 #RANLIB=$(which ranlib)
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "please install mold linker for quicker (and saner) setup"
-    cmake -DCMAKE_LINKER_TYPE=MOLD -DCMAKE_CXX_COMPILER_AR=/usr/bin/ar -DCMAKE_C_COMPILER_AR=/usr/bin/ar -DCMAKE_BUILD_TYPE=Release \
+    cd mujoco && cmake -DCMAKE_LINKER_TYPE=MOLD -DCMAKE_CXX_COMPILER_AR=/usr/bin/ar -DCMAKE_C_COMPILER_AR=/usr/bin/ar -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_COMPILER_RANLIB=/usr/bin/ranlib -DCMAKE_C_COMPILER_RANLIB=/usr/bin/ranlib  -DCMAKE_INSTALL_PREFIX=/usr \
         -B build \
         -G Ninja \
-        -Wno-dev && cd ./mujoco/build && ninja && cd ../../
+        -Wno-dev && cd ./build && ninja && cd ../../
 else
     cd ./mujoco &&  cmake -B build -S . -DBUILD_SHARED_LIBS:BOOL=true -DMUJOCO_HARDEN:BOOL=OFF -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION:BOOL=OFF -DMUJOCO_BUILD_EXAMPLES:BOOL=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=false && cmake --build build --parallel --config=Release;
 fi
